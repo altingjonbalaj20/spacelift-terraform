9827ebee7c901acad1388faeb3114363db409d3e
altingjonbalaj20 <gjonbalaj2017@gmail.com>
2024-08-01 01:20:10 +0200

    Testing module on spacelift

---
+++ tenant2/main.tf
@@ -0,0 +1,23 @@
+module "test" {
+  source = "../modules/initial-module"
+  
+  content = "hello"
+}
+
+module "test4" {
+  source = "../modules/initial-module"
+  
+  content = "hello"
+}
+
+module "test2" {
+  source = "../modules/initial-module"
+  
+  content = "hello"
+}
+
+module "test3" {
+  source = "../modules/initial-module"
+  
+  content = "hello"
+}
