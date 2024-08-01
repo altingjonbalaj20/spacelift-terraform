e0eab345d8435a10dfe0e8030a8924053332fd42
altingjonbalaj20 <gjonbalaj2017@gmail.com>
2024-08-01 15:11:00 +0200

    adding ag & opyn

---
+++ production/opyn/main.tf
@@ -0,0 +1,31 @@
+module "test" {
+  source  = "spacelift.io/llama/spacelift-terraform/default"
+  version = "0.1.0"
+
+  
+  content = "hello"
+}
+
+module "test2" {
+  source  = "spacelift.io/llama/spacelift-terraform/default"
+  version = "0.1.0"
+
+  
+  content = "hello"
+}
+
+module "test3" {
+  source  = "spacelift.io/llama/spacelift-terraform/default"
+  version = "0.1.0"
+
+  
+  content = "hello"
+}
+
+module "test4" {
+  source  = "spacelift.io/llama/spacelift-terraform/default"
+  version = "0.1.0"
+
+  
+  content = "hello"
+}
